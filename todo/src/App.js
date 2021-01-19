e805951aa18a64787943f651b391149b64a003bf
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-19 17:56:56 -0500

    injesting todo data.

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,19 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [{
+    title: "todo 1",
+    id: 0,
+    completed: false
+  },{
+    title: "todo 2",
+    id: 1,
+    completed: false
+  },{
+    title: "todo 3",
+    id: 2,
+    completed: false
+  }];
+
   return (
     <div className="App">
       <h1>ToDo App</h1>
+
       <div className="todoList">
         <h3>List of Items</h3>
         <ul>
-          <li>todo 1</li>
-          <li>todo 2</li>
-          <li>todo 3</li>
+          {
+            todos.map(todo =>{
+              return(<li>{todo.title}</li>);
+            })
+          }
         </ul>
         <button>Clear Completed</button>
       </div>
-      
+
       <div className="todoForm">
         <form>
           <label>
