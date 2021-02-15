096f15edad6b7083d7aaa5ef7488376128e9a905
Trey <treywroberts@gmail.com>
2021-02-15 17:18:33 -0500

    I think MVP is good to go

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
-
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, SET_MEMORY, CLEAR_MEMORY, ADD_MEMORY } from './../actions';
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
-
 const calculateResult = (num1, num2, operation) => {
     switch(operation) {
         case("+"):
@@ -16,7 +14,6 @@ const calculateResult = (num1, num2, operation) => {
             return num1 - num2;
     }
 }
-
 const reducer = (state, action) => {
     switch(action.type) {
         case(ADD_ONE):
@@ -24,22 +21,38 @@ const reducer = (state, action) => {
                 ...state,
                 total: state.total + 1
             });
-
         case(APPLY_NUMBER):
             return ({ 
                 ...state, 
                 total: calculateResult(state.total, action.payload, state.operation)
             });
-        
         case(CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
             });
-            
+        case(CLEAR_DISPLAY):
+            return({
+                ...state,
+                total: 0
+            })
+        case(SET_MEMORY):
+            return({
+                ...state,
+                memory:state.total
+            })
+        case(CLEAR_MEMORY):
+            return({
+                ...state,
+                memory: 0
+            })
+        case(ADD_MEMORY):
+            return ({ 
+                ...state, 
+                memory: state.total + state.memory
+            });
         default:
             return state;
     }
 }
-
 export default reducer;
