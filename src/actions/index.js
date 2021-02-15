c4c413ee4c6ee7e7b86d3b21a5e896a0e8eb4882
CaseyBlatz <caseyblatz@yahoo.com>
2021-02-15 16:14:25 -0700

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,46 @@
-export const ADD_ONE = "ADD_ONE";
 
+export const CLEAR_DISPLAY= 'CLEAR_DISPLAY'
+export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const ADD_MEMORY= 'ADD_TO_MEMORY';
+export const UPDATE_MEMORY= 'UPDATE_MEMORY';
+export const CLEAR_MEMORY= 'CLEAR_MEMORY';
 
-export const addOne = () => {
-    return({type:ADD_ONE});
+const addOne = () => {
+    return({type:ADD_ONE})
 }
 
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
+const applyNumber = (number) => {
+    return({type:APPLY_NUMBER, payload:number})
+}
+
+const changeOperation = (operation) => {
+    return({type:CHANGE_OPERATION, payload:operation})
+}
+
+const clearDisplay = ()=>{
+    return({type: CLEAR_DISPLAY})
+}
+
+const addMemory = ()=>{
+    return({type: ADD_MEMORY})
+}
+
+const updateMemory = ()=>{
+    return({type: UPDATE_MEMORY})
+}
+
+const clearMemory = ()=>{
+    return({type: CLEAR_MEMORY})
+}
+
+export default {
+    addOne,
+    applyNumber,
+    changeOperation,
+    clearDisplay,
+    addMemory,
+    updateMemory,
+    clearMemory,
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
-
+import CalcButton from '../components/CalcButton';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, ADD_MEMORY, UPDATE_MEMORY, CLEAR_MEMORY} from './../actions';
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: '+',
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -14,6 +14,8 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
+            default:
+            return initialState.operation
     }
 }
 
@@ -29,6 +31,7 @@ const reducer = (state, action) => {
             return ({ 
                 ...state, 
                 total: calculateResult(state.total, action.payload, state.operation)
+                // total: Number(state.total + '' + action.payload)
             });
         
         case(CHANGE_OPERATION):
@@ -36,6 +39,29 @@ const reducer = (state, action) => {
                 ...state,
                 operation: action.payload
             });
+            case(CLEAR_DISPLAY):
+            return({
+            ...state,
+            total: 0
+            })
+
+        case(ADD_MEMORY):    
+            return({
+            ...state,
+            memory: state.total
+            })
+
+        case(UPDATE_MEMORY):    
+            return({
+            ...state,
+            memory: state.total + state.memory
+            })
+
+        case(CLEAR_MEMORY):    
+            return({
+            ...state,
+            memory: 0
+            })
             
         default:
             return state;
