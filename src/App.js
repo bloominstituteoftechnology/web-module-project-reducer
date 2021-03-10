81296d635e5fe7e7aad1a9af89ee7f9ae60c93f7
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-10 18:58:19 -0500

    use applyNumber action in app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import { applyNumber } from './actions'
 import reducer, { initialState } from './reducers'
 
 function App() {
@@ -34,21 +35,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => dispatch(applyNumber(1))} />
+              <CalcButton value={2} onClick={() => dispatch(applyNumber(2))} />
+              <CalcButton value={3} onClick={() => dispatch(applyNumber(3))} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => dispatch(applyNumber(4))} />
+              <CalcButton value={5} onClick={() => dispatch(applyNumber(5))} />
+              <CalcButton value={6} onClick={() => dispatch(applyNumber(6))} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => dispatch(applyNumber(7))} />
+              <CalcButton value={8} onClick={() => dispatch(applyNumber(8))}/>
+              <CalcButton value={9} onClick={() => dispatch(applyNumber(9))} />
             </div>
 
             <div className="row">
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
 
 export const initialState = {
     total: 0,
-    operation: "*",
+    operation: "+",
     memory: 0
 }
 
@@ -26,6 +26,7 @@ const reducer = (state, action) => {
             });
 
         case(APPLY_NUMBER):
+            console.log('Apply Number')
             return ({ 
                 ...state, 
                 total: calculateResult(state.total, action.payload, state.operation)
