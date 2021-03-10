2d0250dbbff9a4f5f2c9265a65cb0e0c7afced80
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-10 18:45:26 -0500

    connect reducer state to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
-function App() {
+import reducer, { initialState } from './reducers'
 
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png" alt="Lambda logo"/> Lambda Reducer Challenge</a>
       </nav>
 
       <div className = "container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
