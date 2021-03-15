e2097c9fd08b6befb5fffa63de3f1a0b062591bd
DJcri <djcri123@gmail.com>
2021-03-15 14:26:08 -0700

    completed mvp

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,15 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+
+onClick is triggered
+
+dispatcher is called with action creator arg addOne
+
+reducer executes with param of "ADD_ONE" as action type
+
+state is updated and rerendered
+
+- TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,62 +1,107 @@
-import React from 'react';
+import React, { useReducer } from "react";
 
-import './App.css';
+import "./App.css";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+
+import reducer, { initialState } from "./reducers";
+import {
+  addOne,
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  setMemory,
+  applyMemory,
+  clearMemory,
+} from "./actions";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleClick = (value) => {
+    return (e) => {
+      typeof value === "string"
+        ? dispatch(changeOperation(value))
+        : dispatch(applyNumber(value));
+    };
+  };
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton
+                onClick={(e) => {
+                  dispatch(setMemory());
+                }}
+                value={"M+"}
+              />
+              <CalcButton
+                onClick={(e) => {
+                  dispatch(applyMemory());
+                }}
+                value={"MR"}
+              />
+              <CalcButton
+                onClick={(e) => {
+                  dispatch(clearMemory());
+                }}
+                value={"MC"}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={handleClick(1)} value={1} />
+              <CalcButton onClick={handleClick(2)} value={2} />
+              <CalcButton onClick={handleClick(3)} value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={handleClick(4)} value={4} />
+              <CalcButton onClick={handleClick(5)} value={5} />
+              <CalcButton onClick={handleClick(6)} value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={handleClick(7)} value={7} />
+              <CalcButton onClick={handleClick(8)} value={8} />
+              <CalcButton onClick={handleClick(9)} value={9} />
+            </div>
+
+            <div className="row">
+              <CalcButton onClick={handleClick("+")} value={"+"} />
+              <CalcButton onClick={handleClick("*")} value={"*"} />
+              <CalcButton onClick={handleClick("-")} value={"-"} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton
+                onClick={() => {
+                  dispatch(clearDisplay());
+                }}
+                value={"CE"}
+              />
             </div>
-
           </form>
         </div>
       </div>
