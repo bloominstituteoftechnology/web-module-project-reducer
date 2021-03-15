5af58e426320f131618b728126b0d8c053d5c9ed
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-15 14:48:59 -0700

    all numbers are connected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useReducer} from 'react';
 //REDUCER
 import reducer, {initialState} from './reducers/index'
 //ACTIONS
-import {addOne} from './actions/index'
+import {applyNumber} from './actions/index'
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
@@ -35,22 +35,38 @@ function App() {
 
             <div className="row">
               <CalcButton value={1} onClick={()=>{
-                dispatch(addOne(1))
+                dispatch(applyNumber(1))
               }}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={2} onClick={()=>{
+                dispatch(applyNumber(2))
+              }}/>
+              <CalcButton value={3} onClick={()=>{
+                dispatch(applyNumber(3))
+              }}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={()=>{
+                dispatch(applyNumber(4))
+              }}/>
+              <CalcButton value={5} onClick={()=>{
+                dispatch(applyNumber(5))
+              }}/>
+              <CalcButton value={6} onClick={()=>{
+                dispatch(applyNumber(6))
+              }}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={()=>{
+                dispatch(applyNumber(7))
+              }}/>
+              <CalcButton value={8} onClick={()=>{
+                dispatch(applyNumber(8))
+              }}/>
+              <CalcButton value={9} onClick={()=>{
+                dispatch(applyNumber(9))
+              }}/>
             </div>
 
             <div className="row">
