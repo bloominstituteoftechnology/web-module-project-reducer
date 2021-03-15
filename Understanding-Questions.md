0fc720c6e183d49664fbf0d3a20ad4e37aab0b76
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-15 14:42:49 -0700

    first comit and finished MVP

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,13 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+* the onClick is invoked and the anonimous function runs the function dispatch(that in turn finds a callback addOne(that needs a value that we say is 1))
+
+*it finds addOne in ./actions/index.js where it needs to find what type:ADD_ONE is 
+
+*it goes to  '../reducers/index.js' to find the case of ADD_ONE and sees that it updates state by spreading in  the old value of state, and saying htat the value of total: is state.total incramented by one
+
+*state.total is back on App.js where the totalDisplay value is not updated to be {the old state + 1} 
+
+* TotalDisplay shows the total plus 1. WIN!
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY'
+export const M_PLUS = 'M_PLUS'
+export const M_REPLACE = 'M_REPLACE'
+export const M_CLEAR = 'M_CLEAR'
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperator = (operator) => {
+    return({type: CHANGE_OPERATION, payload:operator })
+}
+
+export const clearDisplay = () => {
+    return ({type: CLEAR_DISPLAY})
+}
+
+export const mPlus = () => {
+    return({type: M_PLUS})
+}
+
+export const mReplace = () => {
+    return({type: M_REPLACE})
+}
+
+export const mClear = () => {
+    return({type: M_CLEAR})
 }
