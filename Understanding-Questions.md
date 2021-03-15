64c9284882b12271f4d45fd6843381dc41bc6eff
Weston Woodard <westonwoodard28@gmail.com>
2021-03-15 14:18:41 -0700

    connect a premade action and connect a better premade action complete

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,9 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
-
+* event object is generated
+* onclick anonymous function fires
+* dispatch goes to reducer
+* reducer looks at case ADD_ONE
+* ADD_ONE looks into actions
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, {initialState} from './reducers';
 import './App.css';
-
+import {applyNumber} from './actions';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+
 function App() {
 
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state, dispatch);
+
+  const eventHandler = (num) => {
+    dispatch(applyNumber(num))
+  }
+
+  
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -17,10 +26,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,21 +39,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={ () => eventHandler(1)}/>
+              <CalcButton value={2} onClick={ () => eventHandler(2)}/>
+              <CalcButton value={3} onClick={ () => eventHandler(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={ () => eventHandler(4)}/>
+              <CalcButton value={5} onClick={ () => eventHandler(5)}/>
+              <CalcButton value={6} onClick={ () => eventHandler(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={ () => eventHandler(7)}/>
+              <CalcButton value={8} onClick={ () => eventHandler(8)}/>
+              <CalcButton value={9} onClick={ () => eventHandler(9)}/>
             </div>
 
             <div className="row">
