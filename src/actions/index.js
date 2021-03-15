8e3c0a60c6b91054ec6bc3953ca0a8df3c89b467
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-15 17:17:02 -0400

    Add memory feature working

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,29 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const ADD_MEMORY = "ADD_MEMORY";
 
-export const addOne = () => {
+const addOne = () => {
     return({type:ADD_ONE});
 }
-
-export const applyNumber = (number) => {
+const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload: operator})
+}
+const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY, payload: 0})
+}
+const addMemory = () => {
+    return({type:ADD_MEMORY})
+}
+
+export default {
+    addOne, 
+    applyNumber,
+    changeOperation,
+    clearDisplay,
+    addMemory,
 }
