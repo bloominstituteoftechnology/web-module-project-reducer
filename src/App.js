2699b35630672b44dcfa2c705ed019a5f5d827b8
hans0 <kiessler.hans@gmail.com>
2021-03-15 16:49:19 -0400

    Initial commit, attached dispatcher

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, { initialState } from './reducers';
+import { applyNumber } from './actions';
 
 import './App.css';
 
@@ -6,6 +8,8 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // console.log(state, dispatch);
 
   return (
     <div className="App">
@@ -17,10 +21,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,15 +34,45 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton 
+                value={1}
+                onClick={(n) => {
+                  dispatch(applyNumber(1))
+                }}
+              />
+              <CalcButton 
+                value={2}
+                onClick={(n) => {
+                  dispatch(applyNumber(2))
+                }}
+              />
+              <CalcButton 
+                value={3}
+                onClick={(n) => {
+                  dispatch(applyNumber(3))
+                }}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton 
+                value={4}
+                onClick={(n) => {
+                  dispatch(applyNumber(4))
+                }}
+              />
+              <CalcButton 
+                value={5}
+                onClick={(n) => {
+                  dispatch(applyNumber(3))
+                }}
+              />
+              <CalcButton  
+                value={6}
+                onClick={(n) => {
+                  dispatch(applyNumber(3))
+                }}
+              />
             </div>
 
             <div className="row">
