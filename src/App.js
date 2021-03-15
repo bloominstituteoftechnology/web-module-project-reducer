7833b0dd7a8fd7ff7a06d51fd70f05ad0284df43
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-03-15 18:08:53 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,67 +1,99 @@
-import React from 'react';
+import React, { useReducer } from "react";
+import {
+	applyNumber,
+	changeOperation,
+	clearDisplay,
+	memoryPlus,
+	memoryClear,
+	memoryRecall,
+} from "../src/actions/index";
 
-import './App.css';
-
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import "./App.css";
+import reducer, { initialState } from "./reducers/index";
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const memoryBank = () => {
+		dispatch(memoryRecall());
+	};
+	const memoryAdd = () => {
+		dispatch(memoryPlus());
+	};
+	const memoryReset = () => {
+		dispatch(memoryClear());
+	};
+	const applyNum = (num) => {
+		dispatch(applyNumber(num));
+	};
+	const clearSlate = () => {
+		dispatch(clearDisplay());
+	};
+	const changeOp = (opp) => {
+		dispatch(changeOperation(opp));
+	};
 
-  return (
-    <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
-      </nav>
+	return (
+		<div className="App">
+			<nav className="navbar navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">
+					<img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+					Challenge
+				</a>
+			</nav>
 
-      <div className = "container row mt-5">
-        <div className="col-md-12 d-flex justify-content-center">
-          <form name="Cal">
-            
-            <TotalDisplay value={0}/>
-            <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
-            </div>
+			<div className="container row mt-5">
+				<div className="col-md-12 d-flex justify-content-center">
+					<form name="Cal">
+						<TotalDisplay value={state.total} />
+						<div className="row details">
+							<span id="operation">
+								<b>Operation:</b> {state.operation}
+							</span>
+							<span id="memory">
+								<b>Memory:</b> {state.memory}
+							</span>
+						</div>
 
-            <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
-            </div>
+						<div className="row">
+							<CalcButton onClick={() => memoryAdd()} value={"M+"} />
+							<CalcButton onClick={() => memoryBank()} value={"MR"} />
+							<CalcButton onClick={() => memoryReset()} value={"MC"} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
-            </div>
+						<div className="row">
+							<CalcButton onClick={() => applyNum(1)} value={1} />
+							<CalcButton onClick={() => applyNum(2)} value={2} />
+							<CalcButton onClick={() => applyNum(3)} value={3} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
-            </div>
+						<div className="row">
+							<CalcButton onClick={() => applyNum(4)} value={4} />
+							<CalcButton onClick={() => applyNum(5)} value={5} />
+							<CalcButton onClick={() => applyNum(6)} value={6} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
-            </div>
+						<div className="row">
+							<CalcButton onClick={() => applyNum(7)} value={7} />
+							<CalcButton onClick={() => applyNum(8)} value={8} />
+							<CalcButton onClick={() => applyNum(9)} value={9} />
+						</div>
 
-            <div className="row ce_button">
-              <CalcButton value={"CE"}/>
-            </div>
+						<div className="row">
+							<CalcButton onClick={() => changeOp("+")} value={"+"} />
+							<CalcButton onClick={() => changeOp("*")} value={"*"} />
+							<CalcButton onClick={() => changeOp("-")} value={"-"} />
+						</div>
 
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+						<div className="row ce_button">
+							<CalcButton onClick={() => clearSlate()} value={"CE"} />
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,31 @@
 export const ADD_ONE = "ADD_ONE";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_PLUS = "MEMORY_PLUS";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+export const MEMORY_RECALL = "MEMORY_RECALL";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+	return { type: ADD_ONE };
+};
+export const memoryPlus = () => {
+	return { type: MEMORY_PLUS };
+};
+export const memoryClear = () => {
+	return { type: MEMORY_CLEAR };
+};
+export const memoryRecall = () => {
+	return { type: MEMORY_RECALL };
+};
+export const clearDisplay = () => {
+	return { type: CLEAR_DISPLAY };
+};
+export const changeOperation = (opp) => {
+	return { type: CHANGE_OPERATION, payload: opp };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+	return { type: APPLY_NUMBER, payload: number };
+};
