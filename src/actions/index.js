458a65c3ea4b17d340072a1f8383388e92f3093c
orlando taylor <ackerie_taylor@hotmail.com>
2021-04-21 20:59:40 -0400

    work in process

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY= 'CLEAR_DISPLAY'
+export const MEM_VALUE = 'MEM_VALUE'
+export const VALUE_MEM ='VALUE_MEM'
+export const MEM_ZERO = ' MEM_ZERO'
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,26 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const operationChanger = (operation) => {
+    return ({type:CHANGE_OPERATION, payload:operation})
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY});
+}
+
+export const memValue = () => {
+    return({type:MEM_VALUE})   
+}
+
+
+export const valueMem = () => {
+    return({type:VALUE_MEM})   
+}
+
+
+export const memZero = () => {
+    return({type:MEM_ZERO}) 
 }
