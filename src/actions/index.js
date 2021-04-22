8de64b18c9bcf230937ac643d2e861ff4d2a6011
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-04-21 21:18:18 -0700

    Project Complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,29 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CHANGE_MEMORY = "CHANGE_MEMORY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 
 export const addOne = () => {
     return({type:ADD_ONE});
 }
 
+export const changeMemory = (memoryOperator) => {
+    console.log("changeMemory action creator fires")
+    return({type:CHANGE_MEMORY, payload: memoryOperator});
+}
+
 export const applyNumber = (number) => {
+    console.log("applyNumber action creator fires")
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    console.log("changeOperation action creator fires")
+    return({type:CHANGE_OPERATION, payload: operator});
+}
+
+export const clearDisplay = () => {
+    console.log("clearDispaly action creator fires")
+    return ({type:CLEAR_DISPLAY});
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, CHANGE_MEMORY, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY } from './../actions';
 
+// export const initialState = {
+//     total: 100,
+//     operation: "*",
+//     memory: 100
+// }
+
+// TEST CASE
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -17,6 +24,23 @@ const calculateResult = (num1, num2, operation) => {
     }
 }
 
+const calculateMemory = (total, memoryOperator) => {
+    switch(memoryOperator) {
+        case("M+"):
+            return total;
+        case("MC"):
+            return 0;
+        case("MR"):
+            console.log("ERROR: Calculate Memory does not handle \"MR\" memoryOperator");
+            return undefined;
+    }
+};
+
+// * [ ] When `M+` is pressed, the current memory value should be set to the current total value. Test by seeing the result of memory in the UI.
+// * [ ] When `MR` is pressed, the current memory value should be applied to the current total value(See the APPLY_NUMBER case). Test by adding a value to memory and then seeing if the total updates correctly when pressed.
+// * [ ] When `MC` is pressed, the current memory value should be set to zero. Test by adding a value to memory and then seeing the memory value reset to zero when pressed.
+
+
 const reducer = (state, action) => {
     switch(action.type) {
         case(ADD_ONE):
@@ -25,16 +49,37 @@ const reducer = (state, action) => {
                 total: state.total + 1
             });
 
+        case(CHANGE_MEMORY):
+            const memoryOperator = action.payload;
+            if (memoryOperator === "MR") {
+                return ({
+                    ...state,
+                    total: state.memory
+                })
+            } else {
+                return ({
+                    ...state,
+                    memory: calculateMemory(state.total, action.payload)
+                })
+            };
+
         case(APPLY_NUMBER):
+        // console.log("Type of payload:", typeof(action.payload)) STRING! 
             return ({ 
                 ...state, 
-                total: calculateResult(state.total, action.payload, state.operation)
+                total: calculateResult(state.total, Number(action.payload), state.operation)
             });
         
         case(CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
+            });
+        
+        case(CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
             });
             
         default:
