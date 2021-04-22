e324a44abf909eb5be856cedadbb30b206e24bfa
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-04-22 03:30:56 -0700

    worked on project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-
+import React, {useState, useReducer} from 'react';
+import {ADD_ONE} from './actions/index'
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, {initialState} from './reducers/index'
 
 function App() {
-
+const [state, dispatch] = useReducer(reducer, initialState);
+const handleClick = () =>{
+  dispatch({type: "ADD_ONE", payload: 1})
+}
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -17,10 +21,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
@@ -30,7 +34,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton onClick = {handleClick} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
