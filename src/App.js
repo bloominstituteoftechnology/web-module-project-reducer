bde4f29b045261012ca5bf13d5020ca300a8f55d
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-04-21 20:15:35 -0400

    already completely lost

--- src/App.js
+++ src/App.js
@@ -1,62 +1,74 @@
-import React from 'react';
-
-import './App.css';
-
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import React, { useReducer } from "react";
+import index from "./reducers/index";
+import "./App.css";
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 function App() {
+
+const initialState = {
+    total: 100,
+    operation: "*",
+    memory: 100
+}
+
+  const [state, dispatch] = useReducer(index, initialState);
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={0} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> X
+              </span>
+              <span id="memory">
+                <b>Memory:</b> 0
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} />
+              <CalcButton value={"MR"} />
+              <CalcButton value={"MC"} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={1} />
+              <CalcButton value={2} />
+              <CalcButton value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} />
+              <CalcButton value={5} />
+              <CalcButton value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} />
+              <CalcButton value={8} />
+              <CalcButton value={9} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} />
+              <CalcButton value={"*"} />
+              <CalcButton value={"-"} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} />
             </div>
-
           </form>
         </div>
       </div>
