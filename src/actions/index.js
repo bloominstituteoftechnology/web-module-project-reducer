20b1a8dc6b3f2730b204d1c14d4eaa1c5f8e15f5
Anwar <AnwarAldogmi@gmail.com>
2021-05-10 19:40:31 -0700

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+export const CHANGE_MEMORY = 'CHANGE_MEMORY';
+export const CLEAR_MEMORY = 'CLEAR_MEMORY';
+export const RECALL_MEMORY = 'RECALL_MEMORY';
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (string) => {
+    return({type:CHANGE_OPERATION, payload:string});
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY});
+}
+
+export const changeMemory = () => {
+    return({type:CHANGE_MEMORY});
+}
+
+export const clearMemory = () => {
+    return({type:CLEAR_MEMORY});
+}
+
+export const recallMemory = () => {
+    return({type:RECALL_MEMORY});
 }
