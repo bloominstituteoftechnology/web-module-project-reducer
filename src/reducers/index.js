005f765c4e5b970ca9254d49d0872f4af33f4b8a
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-05-19 20:50:34 -0400

    mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CHANGE_MEMORY, CLEAR_DISPLAY}  from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -14,32 +14,61 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
-    }
+}
+}
+const calculateMemory = (total, memoryOp) =>{
+switch (memoryOp){
+    case("M+"):
+        return total;
+    case("MC"):
+        return(0);
+    case ("MR"):
+        return undefined;
+}
 }
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
+switch(action.type) {
+    case(ADD_ONE):
             return({
                 ...state,
                 total: state.total + 1
-            });
+});
 
-        case(APPLY_NUMBER):
+    case(APPLY_NUMBER):
             return ({ 
                 ...state, 
                 total: calculateResult(state.total, action.payload, state.operation)
-            });
+});
         
-        case(CHANGE_OPERATION):
+    case(CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
-            });
-            
-        default:
-            return state;
-    }
+});
+    case (CHANGE_MEMORY):
+        
+    const memoryOperator = action.payload;
+        if(memoryOperator === "MR"){
+            return ({
+                ...state,
+            total: state.memory
+})
 }
+        else {
+            return ({
+                ...state,
+                memory: calculateMemory(state.total,action.payload)
+})
+}
+    case(CLEAR_DISPLAY):
+        return ({
+            ...state,
+            total:0
+            });
 
+default:
+return state;
+
+}}
 export default reducer;
