1a1945355f5d4befdfdee82ed8d8ff73de8e65ea
Tawnya Wessar <contact-tdubs@protonmail.com>
2021-05-29 15:06:35 -0700

    mvp complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,36 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
-export const addOne = () => {
-    return({type:ADD_ONE});
-}
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 
-export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+export const SET_MEMORY = "SET_MEMORY";
+export const APPLY_MEMORY = "APPLY_MEMORY";
+export const CLEAR_MEMORY = "CLEAR_MEMORY";
+
+export const addOne = () => {
+    return ( { type: ADD_ONE } );
+};
+
+export const applyNumber = ( number ) => {
+    return ( { type: APPLY_NUMBER, payload: number } );
+};
+
+export const applyOperation = ( operation ) => {
+    return ( { type: CHANGE_OPERATION, payload: operation } );
+};
+
+export const clearDisplay = () => {
+    return ( { type: CLEAR_DISPLAY } );
+};
+
+export const applyMemory = () => {
+    return ( { type: APPLY_MEMORY } );
+};
+
+export const setMemory = () => {
+    return ( { type: SET_MEMORY } );
+};
+
+export const clearMemory = () => {
+    return ( { type: CLEAR_MEMORY } );
+};
