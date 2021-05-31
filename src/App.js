7976e83b29f4bd869ceef1b7ac49bc816f4e3dc7
Matthew Larson <maflarson@gmail.com>
2021-05-30 18:55:14 -0600

    sdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
-
+import reducer, {initialState} from "./reducers/index"
+import {addOne, applyNumber, changeOpperation, clearDisplay, setMemoryToTotal, setTotalToMemory, clearMemory} from "./actions/index"
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
 
+  const handleClearClick = ()=>{
+    dispatch(clearDisplay());
+  }
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -17,44 +22,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick = {()=> {dispatch(setMemoryToTotal())}} value={"M+"}/>
+              <CalcButton onClick = {()=> {dispatch(setTotalToMemory())}} value={"MR"}/>
+              <CalcButton onClick = {()=> {dispatch(clearMemory())}} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick = {()=> {dispatch(applyNumber(1))}} value={1}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(2))}} value={2}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(3))}} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(4))}} value={4}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(5))}} value={5}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(6))}} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(7))}} value={7}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(8))}} value={8}/>
+              <CalcButton onClick = {() => {dispatch(applyNumber(9))}} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick = {() => {dispatch(changeOpperation("+"))}}value={"+"}/>
+              <CalcButton onClick = {() => {dispatch(changeOpperation("-"))}}value={"-"}/>
+              <CalcButton onClick = {() => {dispatch(changeOpperation("*"))}}value={"*"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick = {handleClearClick} value={"CE"}/>
             </div>
 
           </form>
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,11 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const SET_MEMORY_TO_TOTAL = "SET_MEMORY_TO_TOTAL";
+export const SET_TOTAL_TO_MEMORY = "SET_TOTAL_TO_MEMORY";
+export const CLEAR_MEMORY = "CLEAR_MEMORY";
+
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +14,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOpperation = (op) => {
+    return({type:CHANGE_OPERATION, payload:op})
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
+}
+
+export const setMemoryToTotal = () => {
+    return({type:SET_MEMORY_TO_TOTAL})
+}
+
+export const setTotalToMemory = () => {
+    return({type:SET_TOTAL_TO_MEMORY})
+}
+
+export const clearMemory = () => {
+    return({type:CLEAR_MEMORY})
 }
