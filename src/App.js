76e667493f7d9ad218d28fa59e11115d0d7eab22
Karla Nieves <karlaxnieves@gmail.com>
2021-06-07 15:09:35 -0700

    created a handle event for onClick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useReducer} from 'react';
 import reducer, {initialState} from './reducers'
-import {addOne, applyNumber} from './actions'
+import {applyNumber} from './actions'
 
 import './App.css';
 
@@ -10,8 +10,8 @@ import CalcButton from './components/CalcButton';
 function App() {
   const [state, dispatch ] = useReducer(reducer, initialState);
 
-  const handleAdd = () =>{
-    dispatch(addOne())
+  const handleNumber = (num) =>{
+    dispatch(applyNumber(num))
   }
 
   return (
@@ -37,21 +37,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick ={handleAdd} value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={ () => {handleNumber(1)}}/>
+              <CalcButton value={2} onClick={ () => {handleNumber(2)}}/>
+              <CalcButton value={3} onClick={ () => {handleNumber(3)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={ () => {handleNumber(4)}}/>
+              <CalcButton value={5} onClick={ () => {handleNumber(5)}}/>
+              <CalcButton value={6} onClick={ () => {handleNumber(6)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={ () => {handleNumber(7)}}/>
+              <CalcButton value={8} onClick={ () => {handleNumber(8)}}/>
+              <CalcButton value={9} onClick={ () => {handleNumber(9)}}/>
             </div>
 
             <div className="row">
