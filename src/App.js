af83ff20e04bf3ddbd60c4c346505bcd0ce9fc78
Karla Nieves <karlaxnieves@gmail.com>
2021-06-07 14:50:41 -0700

    created an event handler that adds one

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useReducer} from 'react';
 import reducer, {initialState} from './reducers'
+import {addOne, applyNumber} from './actions'
 
 import './App.css';
 
@@ -8,6 +9,10 @@ import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch ] = useReducer(reducer, initialState);
+
+  const handleAdd = () =>{
+    dispatch(addOne())
+  }
 
   return (
     <div className="App">
@@ -21,7 +26,7 @@ function App() {
             
             <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> state.operation</span>
+              <span id="operation"><b>Operation:</b>state.operation</span>
               <span id="memory"><b>Memory:</b>state.memory</span>
             </div>
             
@@ -32,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton onClick ={handleAdd} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
 
 export const initialState = {
-    total: 100,
+    total: 0,
     operation: "*",
-    memory: 100
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
