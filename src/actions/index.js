9b20e125a81387753859ef862ec9cca7298f9d64
Lirong <lirongcoding@gmail.com>
2021-06-16 23:54:51 -0700

    mvp1st

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,11 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_APPLY = "MEMORY_APPLY";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
+
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +14,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changOperation = (operator) => {
+    return ({type:CHANGE_OPERATION, payload:operator});
+}
+
+export const clearDisplay = () => {
+    return ({type:CLEAR_DISPLAY});
+}
+
+export const memoryAdd = () => {
+    return ({type:MEMORY_ADD});
+}
+
+export const memoryClear = () => {
+    return ({type:MEMORY_CLEAR});
+}
+
+export const memoryApply = () => {
+    return ({type:MEMORY_APPLY});
 }
