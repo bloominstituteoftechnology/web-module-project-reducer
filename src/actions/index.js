b0faa5fe9c68c56436c4700c63357bcf113cf867
michaelquintdev <michaelquintdev@gmail.com>
2021-07-12 16:58:06 -0500

    done with project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_RELEASE = "MEMORY_RELEASE";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,34 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload: operator})
+}
+
+export const clearDisplay = () => {
+    return({type: CLEAR_DISPLAY})
+}
+
+export const memoryAdd = () => {
+    return({type: MEMORY_ADD})
+}
+
+export const memoryRelease = () => {
+    return({type: MEMORY_RELEASE})
+}
+
+export const memoryClear = () => {
+    return({type: MEMORY_CLEAR})
+}
+
+export default {
+    addOne,
+    applyNumber,
+    changeOperation,
+    clearDisplay,
+    memoryAdd,
+    memoryRelease,
+    memoryClear,
 }
