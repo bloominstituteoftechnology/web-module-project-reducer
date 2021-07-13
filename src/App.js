9e3fccb61b504d62f2c836f14fb52a9e4b290e4e
ferengitooth <ehitsamill@gmail.com>
2021-07-12 18:05:29 -0700

    displayed state to UI

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
-  console.log(state)
+
 
   return (
     <div className="App">
@@ -23,10 +23,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
