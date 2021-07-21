cb0dbd07557d5873d72d8309ba4e07087738aaad
Angela Engel <amengel.ae@icloud.com>
2021-07-21 15:27:09 -0600

    changes made

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_TOTAL = "CLEAR_TOTAL"
+export const CLEAR_MEMORY = "CLEAR_MEMORY"
+export const ADD_TO_MEMORY = "ADD_TO_MEMORY"
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,19 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return({type: CHANGE_OPERATION, payload: operation})
+}
+export const clear = () => {
+    return({type: CLEAR_TOTAL})
+}
+
+export const clearMemory = () => {
+    return({type: CLEAR_MEMORY})
+}
+
+export const addToMemory = (number) => {
+    return ({type: ADD_TO_MEMORY, payload: number })
 }
