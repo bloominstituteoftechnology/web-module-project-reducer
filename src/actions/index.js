d74d96f72881d1e565517c6a93e1fd5c6b7a24d5
Kyle <kyle.e.cardwell@gmail.com>
2021-07-21 21:03:00 -0600

    completed all tasks through CLEAR_DISPLAY

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY"
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +10,12 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload:operator})
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
 }
