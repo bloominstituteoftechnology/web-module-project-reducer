bb5b3186fb4edd43d391174d7378608418f16a80
TrevourGuild <trevorr.guildd@gmail.com>
2021-08-09 14:22:34 -0700

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, {initialState} from './reducers/index';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import {addOne, applyNumber, changeOperation, clearDisplay, setMemoryTotatl, applyMemoryTotal, setMemoryZero} from './actions/index'
+
+
 function App() {
+
+  const [state, dispatch ] = useReducer(reducer, initialState)
+
+  // const handleAddClick = () =>{
+  //   dispatch(addOne(1))
+  // }
+
+  const handleApplyClickOne = () =>{
+    dispatch(applyNumber(1))
+  }
+  const handleApplyClickTwo = () =>{
+    dispatch(applyNumber(2))
+  }
+  const handleApplyClickThree = () =>{
+    dispatch(applyNumber(3))
+  }
+  const handleApplyClickFour = () =>{
+    dispatch(applyNumber(4))
+  }
+  const handleApplyClickFive = () =>{
+    dispatch(applyNumber(5))
+  }
+  const handleApplyClickSix = () =>{
+    dispatch(applyNumber(6))
+  }
+  const handleApplyClickSeven = () =>{
+    dispatch(applyNumber(7))
+  }
+  const handleApplyClickEight = () =>{
+    dispatch(applyNumber(8))
+  }
+  const handleApplyClickNine = () =>{
+    dispatch(applyNumber(9))
+  }
+ 
+  const handleOperationAddClick = () =>{
+    dispatch(changeOperation('+'))
+  }
+
+  const handleOperationMultiplyClick = () =>{
+    dispatch(changeOperation('*'))
+  }
+
+  const handleOperationSubClick = () =>{
+    dispatch(changeOperation('-'))
+  }
+
+  const handleClearClick= () => {
+    dispatch(clearDisplay())
+  }
+
+  const handleSetMemoryClick = () =>{
+    dispatch(setMemoryTotatl())
+  }
+
+  const handleApplyMemoryClick = () =>{
+    dispatch(applyMemoryTotal())
+  }
+
+  const handleClearMemoryClick = () =>{
+    dispatch(setMemoryZero())
+  }
 
   return (
     <div className="App">
@@ -17,44 +83,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick = {handleSetMemoryClick}value={"M+"}/>
+              <CalcButton onClick = {handleApplyMemoryClick}value={"MR"}/>
+              <CalcButton onClick = {handleClearMemoryClick}value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick = {handleApplyClickOne}value={1}/>
+              <CalcButton onClick = {handleApplyClickTwo}value={2}/>
+              <CalcButton onClick = {handleApplyClickThree}value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick = {handleApplyClickFour}value={4}/>
+              <CalcButton onClick = {handleApplyClickFive}value={5}/>
+              <CalcButton onClick = {handleApplyClickSix}value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick = {handleApplyClickSeven}value={7}/>
+              <CalcButton onClick = {handleApplyClickEight}value={8}/>
+              <CalcButton onClick = {handleApplyClickNine}value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick = {handleOperationAddClick}value={"+"}/>
+              <CalcButton onClick = {handleOperationMultiplyClick}value={"*"}/>
+              <CalcButton onClick = {handleOperationSubClick}value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick = {handleClearClick}value={"CE"}/>
             </div>
 
           </form>
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+export const SET_MEMORY_TOTAL = 'SET_MEMORY_TOTAL';
+export const APPLY_MEMORY_TOTAL = 'APPLY_MEMORY_TOTAL';
+export const SET_MEMORY_ZERO = 'SET_MEMORY_ZERO';
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +13,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) =>{
+    return({type: CHANGE_OPERATION, payload:operator})
+}
+
+export const clearDisplay = () =>{
+    return ({type: CLEAR_DISPLAY})
+}
+
+export const setMemoryTotatl = () =>{
+    return ({type: SET_MEMORY_TOTAL})
+}
+
+export const applyMemoryTotal = () => {
+    return ({type: APPLY_MEMORY_TOTAL})
+}
+
+export const setMemoryZero = () =>{
+    return ({type: SET_MEMORY_ZERO})
 }
