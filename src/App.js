0dafbe5b26f906785422949b45b8372d20539b82
ylei1088 <ylei1088@gmail.com>
2021-08-09 19:54:58 -0500

    added reducer

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
-import React from 'react';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from "./actions/index";
+import reducer, { initialState } from "./reducers";
 
-import './App.css';
+import React, { useReducer } from "react";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
+import "./App.css";
+
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const APPLY_NUMBER = (int) => {
+    dispatch({ type: APPLY_NUMBER, playload: int });
+  };
+
+  const ADD_ONE = (int) => {
+    dispatch({ type: ADD_ONE, playload: int });
+  };
+
+  const CHANGE_OPERATION = (int) => {
+    dispatch({ type: CHANGE_OPERATION, playload: int });
+  };
 
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} />
+              <CalcButton value={"MR"} />
+              <CalcButton value={"MC"} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={1} />
+              <CalcButton value={2} />
+              <CalcButton value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} />
+              <CalcButton value={5} />
+              <CalcButton value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} />
+              <CalcButton value={8} />
+              <CalcButton value={9} />
+            </div>
+
+            <div className="row">
+              <CalcButton value={"+"} />
+              <CalcButton value={"*"} />
+              <CalcButton value={"-"} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} />
             </div>
-
           </form>
         </div>
       </div>
