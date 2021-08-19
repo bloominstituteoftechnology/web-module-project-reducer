acf59ae9c3874bcdb558b9e2fb2d9807ec506602
Dalian <DalianAlyxander@me.com>
2021-08-19 00:01:45 -0400

    initial commit, added useReducer hook/state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+function reducer() {
+
+}
+
+
 function App() {
+  const [state, dispatch] = useReducer(calcReducer, initialState)
+
+  
 
   return (
     <div className="App">
@@ -17,10 +25,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
