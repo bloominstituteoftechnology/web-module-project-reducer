f76ad272cff17e5114e62dd5b028ed791e8f1917
Gabe Chilson <g.chilson34@gmail.com>
2021-08-18 21:29:52 -0600

    finished most MVP tasks, will begin working on memory functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY } from './../actions';
 
 export const initialState = {
     total: 100,
-    operation: "*",
+    operation: "+",
     memory: 100
 }
 
@@ -17,7 +17,7 @@ const calculateResult = (num1, num2, operation) => {
     }
 }
 
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
     switch(action.type) {
         case(ADD_ONE):
             return({
@@ -35,6 +35,12 @@ const reducer = (state, action) => {
             return ({
                 ...state,
                 operation: action.payload
+            });
+
+        case(CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
             });
             
         default:
