d10b942c741f384aba59a13574a72c9894edb9f9
Dalian <DalianAlyxander@me.com>
2021-08-20 23:29:10 -0400

    added clickHandler for the 1 button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 import reducer, { initialState } from './reducers';
+import { addOne } from './actions';
 
 
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  const handleOneClick = () => {
+    dispatch(addOne())
+  }
   
   return (
     <div className="App">
@@ -35,7 +39,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleOneClick}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
