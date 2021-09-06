3e79ede3b6bc2cbe35cea384bdeab70a42b59307
Brandon May <battleb2014@gmail.com>
2021-09-06 14:24:27 -0400

    done

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,10 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- Onclick handleAddOne is fired
+- Dispatch is fired inside of the handleAddOne function, taking in the addOne function as an argument
+- AddOne function returns the type "ADD_ONE", referencing the case in the reducer
+- ADD_ONE case returns spread state and sets total to state.total + 1
+- TotalDisplay shows the total plus 1.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,41 +1,65 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, MEMORY_ADD, MEMORY_CLEAR, MEMORY_RETRIEVE } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
-const calculateResult = (num1, num2, operation) => {
-    switch(operation) {
-        case("+"):
+const calculateResult = ( num1, num2, operation ) => {
+    switch( operation ) {
+        case ("+" ):
             return num1 + num2;
-        case("*"):
+        case( "*" ):
             return num1 * num2;
-        case("-"):
+        case( "-" ):
             return num1 - num2;
     }
 }
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
+const reducer = ( state, action ) => {
+    switch( action.type ) {
+        case( ADD_ONE ):
             return({
                 ...state,
                 total: state.total + 1
             });
 
-        case(APPLY_NUMBER):
+        case( APPLY_NUMBER ):
             return ({ 
                 ...state, 
-                total: calculateResult(state.total, action.payload, state.operation)
+                total: calculateResult( state.total, action.payload, state.operation )
             });
         
-        case(CHANGE_OPERATION):
+        case( CHANGE_OPERATION ):
             return ({
                 ...state,
                 operation: action.payload
             });
+
+        case( CLEAR_DISPLAY ):
+            return({
+                ...state,
+                total: 0
+            })
+
+        case( MEMORY_ADD ):
+            return({
+                ...state,
+                memory: state.total
+            })
+
+        case( MEMORY_RETRIEVE ):
+            return({
+                ...state,
+                total: state.memory
+            })
+
+        case( MEMORY_CLEAR ):
+            return({
+                ...state,
+                memory: 0
+            })
             
         default:
             return state;
