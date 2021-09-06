0ecb086c6c6cdc5dc7b62d8537f84aaa8947db91
Leo Hays <starryhornet@gmail.com>
2021-09-06 14:47:36 -0500

    Module calc complete

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,10 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- It goes to the event handdler
+- dispatch passes the action to the reducer
+- reducer then dose the action and update total
+- total gets passed to TotalDisplay
+- TotalDisplay shows the total plus 1.
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,36 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+export const CLEAR_DISPLAY  = 'CLEAR_DISPLAY'
+
+export const HOLD_MEMORY = 'HOLD_MEMORY'
+export const MEMORY_CLEAR = 'MEMORY_CLEAR'
+export const MEMORY_RECALL = 'MEMORY_RECALL'
+
 export const addOne = () => {
     return({type:ADD_ONE});
 }
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) => {
+    return({type:CHANGE_OPERATION, payload:operator})
+}
+
+export const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
+}
+
+export const holdMemory = () => {
+    return({type:HOLD_MEMORY})
+}
+
+export const memoryClear = () => {
+    return({type:MEMORY_CLEAR})
+}
+
+export const memoryRecall = () => {
+    return({type:MEMORY_RECALL})
 }
