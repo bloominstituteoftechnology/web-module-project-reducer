654d9947b17dcfaefc42477ad13a8f28efa2f60b
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-09-07 01:08:46 +0100

    improvements

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ console.log("currentState", currentState);
 currentState = reducer(currentState, { type: "ADD", payload: 5 });
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -22,13 +24,13 @@ function App() {
       <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
               <span id="operation">
-                <b>Operation:</b> X
+                <b>Operation:</b> {state.operation}
               </span>
               <span id="memory">
-                <b>Memory:</b> 0
+                <b>Memory:</b> {state.memory}
               </span>
             </div>
 
