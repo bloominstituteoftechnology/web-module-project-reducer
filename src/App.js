45ac458325093dc29ab256768c0313b9f3c6de0a
techwulf <t.zach.moore@gmail.com>
2021-09-07 19:42:48 -0600

    complete mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState } from './reducers';
+import {
+  addOne,
+  applyNumber,
+  changeOperation,
+  clearDisplay,
+  setMemory,
+  applyMemory,
+  clearMemory
+} from './actions';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clicked = (value) => {
+    return () => {
+      typeof value === "string" ? 
+      dispatch(changeOperation(value)) :
+      dispatch(applyNumber(value));
+    }
+  }
 
   return (
     <div className="App">
@@ -17,46 +36,45 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={() => dispatch(setMemory())} value={"M+"}/>
+              <CalcButton onClick={() => dispatch(applyMemory())}value={"MR"}/>
+              <CalcButton onClick={() => dispatch(clearMemory())}value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={clicked(1)} value={1}/>
+              <CalcButton onClick={clicked(2)} value={2}/>
+              <CalcButton onClick={clicked(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={clicked(4)} value={4}/>
+              <CalcButton onClick={clicked(5)} value={5}/>
+              <CalcButton onClick={clicked(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={clicked(7)} value={7}/>
+              <CalcButton onClick={clicked(8)} value={8}/>
+              <CalcButton onClick={clicked(9)} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={clicked('+')} value={"+"}/>
+              <CalcButton onClick={clicked('*')} value={"*"}/>
+              <CalcButton onClick={clicked('-')} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={()=> dispatch(clearDisplay())} value={"CE"}/>
             </div>
-
           </form>
         </div>
       </div>
