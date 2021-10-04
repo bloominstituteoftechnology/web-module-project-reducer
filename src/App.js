293280ce6e469c5ba106b1d5f34be5c37124aafb
Abraman18 <abraman18@gmail.com>
2021-10-04 15:21:57 -0700

    apply not working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, { initialState } from './reducers';
+import { applyNumber, addOne } from './actions';
 
 import './App.css';
 
@@ -6,6 +8,16 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddButton = (value) => {
+    dispatch(addOne(value));
+  }
+
+  const handleApplyNumber = (number) => {
+    dispatch(applyNumber(number));
+  }
 
   return (
     <div className="App">
@@ -17,10 +29,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> { state.memory }</span>
             </div>
             
             <div className="row">
@@ -30,15 +42,15 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={()=>{handleApplyNumber(1)}}/>
+              <CalcButton value={2} />
+              <CalcButton value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} />
+              <CalcButton value={5} />
+              <CalcButton value={6} />
             </div>
 
             <div className="row">
