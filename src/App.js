eb5b6e993e09478d827434d03a9b655bacde7e96
Abraman18 <abraman18@gmail.com>
2021-10-04 16:22:09 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './reducers';
-import { applyNumber, addOne } from './actions';
+import { applyNumber, changeOperation, clearDisplay, memoryPlus, mR, memoryClear } from './actions';
 
 import './App.css';
 
@@ -11,12 +11,32 @@ function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleAddButton = (value) => {
-    dispatch(addOne(value));
-  }
+  // const handleAddButton = (value) => {
+  //   dispatch(addOne(value));
+  // }
 
   const handleApplyNumber = (number) => {
     dispatch(applyNumber(number));
+  }
+
+  const handleOperationChanger = (operation) => {
+    dispatch(changeOperation(operation));
+  }
+
+  const clearDisplayHandler = () => {
+    dispatch(clearDisplay());
+  }
+
+  const memoryPlusHandler = () => {
+    dispatch(memoryPlus());
+  }
+
+  const mRHandler = () => {
+    dispatch(mR());
+  }
+
+  const memoryClearHandler = () => {
+    dispatch(memoryClear());
   }
 
   return (
@@ -36,37 +56,37 @@ function App() {
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={()=>{memoryPlusHandler()}} />
+              <CalcButton value={"MR"} onClick={()=>{mRHandler()}} />
+              <CalcButton value={"MC"} onClick={()=>{memoryClearHandler()}}/>
             </div>
 
             <div className="row">
               <CalcButton value={1} onClick={()=>{handleApplyNumber(1)}}/>
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={2} onClick={() => { handleApplyNumber(2) }}/>
+              <CalcButton value={3} onClick={() => { handleApplyNumber(3) }}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => { handleApplyNumber(4) }}/>
+              <CalcButton value={5} onClick={() => { handleApplyNumber(5) }}/>
+              <CalcButton value={6} onClick={() => { handleApplyNumber(6) }}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => { handleApplyNumber(7) }}/>
+              <CalcButton value={8} onClick={() => { handleApplyNumber(8) }}/>
+              <CalcButton value={9} onClick={() => { handleApplyNumber(9) }}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={ ()=>{handleOperationChanger("+")} }/>
+              <CalcButton value={"*"} onClick={ ()=>{handleOperationChanger("*")} }/>
+              <CalcButton value={"-"} onClick={ ()=>{handleOperationChanger("-")} }/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={()=>{clearDisplayHandler()} }/>
             </div>
 
           </form>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, MEMORY_PLUS, M_R, MEMORY_CLEAR } from './../actions';
 
 export const initialState = {
     total: 0,
-    operation: "*",
+    operation: "+",
     memory: 0
 }
 
@@ -36,6 +36,27 @@ const reducer = (state, action) => {
                 ...state,
                 operation: action.payload
             });
+        
+        case (CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            });
+        case (MEMORY_PLUS):
+            return ({
+                ...state,
+                memory: state.total
+            });
+        case (M_R):
+            return ({
+                ...state,
+                total: state.memory
+            });
+            case (MEMORY_CLEAR):
+                return ({
+                    ...state,
+                    memory: 0
+                });
             
         default:
             return state;
