c8d4a908936adb16d7605965de0ba44f9c0bf12a
jimlemoine <jlemoine007@gmail.com>
2021-10-04 13:12:50 -0700

    connect a premade action complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import reducer, { initialState } from './reducers';
 
 import './App.css';
 
+import { addOne } from './actions';
+
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddOne = e => {
+    dispatch(addOne(e.target.value));
+  }
 
   return (
     <div className="App">
@@ -32,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleAddOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
