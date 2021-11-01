582b5ef64333d604d28e7e1e7776610f141f557c
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-11-01 17:10:21 -0400

    first push

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,6 +14,8 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
+            default: 
+            return num1
     }
 }
 
