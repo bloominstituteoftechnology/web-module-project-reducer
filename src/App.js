1625b91e5fe5a78f3de725162d104a8b36ce8eee
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-11-01 15:53:08 -0700

    MVP done. Working on adding dynamic display and easy to use functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React, { useState, useReducer } from 'react';
+import reducer, { initialState } from './reducers/index';
 import './App.css';
+import { addOne, applyNumber, changeOperation, clearDisplay, memory, applyMemory, clearMemory, equals } from './actions'
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+  const [state,dispatch] = useReducer(reducer, initialState);
+  console.log('Step 1/5: State is rendered to the page')
+
+  const applyNumero = (e) =>{
+    console.log("Step 2: Click handler dispatches addNumero action!")
+    const parsed = parseInt(e.target.textContent)
+    dispatch(applyNumber(parsed))
+  }
+
+  const changeOperator = (e) =>{
+    console.log(e.target.outerText)
+    dispatch(changeOperation(e.target.outerText))
+  }
+
+  const clear = ()=>{
+    dispatch(clearDisplay())
+  }
+
+  const dispatchMemory = () => {
+    dispatch(memory())
+  }
+
+  const appMem = () => {
+    dispatch(applyMemory())
+  }
+
+  const clearMem = () => {
+    console.log("Step 2: onClick")
+    dispatch(clearMemory())
+  }
+
+  const eq = () => {
+    console.log("Step 2: onClick")
+    dispatch(equals())
+  }
+ 
 
   return (
     <div className="App">
@@ -17,44 +54,45 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.display}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={dispatchMemory}/>
+              <CalcButton value={"MR"} onClick={appMem}/>
+              <CalcButton value={"MC"} onClick={clearMem}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick = {applyNumero}/>
+              <CalcButton value={2} onClick={applyNumero}/>
+              <CalcButton value={3} onClick={applyNumero}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={applyNumero}/>
+              <CalcButton value={5} onClick={applyNumero}/>
+              <CalcButton value={6} onClick={applyNumero}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={applyNumero}/>
+              <CalcButton value={8} onClick={applyNumero}/>
+              <CalcButton value={9} onClick={applyNumero}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={changeOperator}/>
+              <CalcButton value={"*"} onClick={changeOperator}/>
+              <CalcButton value={"-"} onClick={changeOperator}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={clear}/>
+              <CalcButton value={"="} onClick={eq}/>
             </div>
 
           </form>
