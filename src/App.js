c8f1ee7ab776f7b5948cf2d66dc9ad9cc1c69daf
Justin Peisker <justinpeisker@gmail.com>
2021-11-01 13:38:32 -0700

    add 1 feature working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
+import React, {useReducer} from 'react';
+import reducer,{ initialState } from './reducers/index';
+// import {addOne} from './actions';
+import { applyNumber } from './actions';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
-function App() {
+export default function App() {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleApplyNumber = () => {
+      dispatch(applyNumber(1))
+    }
+
 
   return (
     <div className="App">
@@ -17,10 +26,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -30,7 +39,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton onClick= {handleApplyNumber} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
@@ -64,4 +73,4 @@ function App() {
   );
 }
 
-export default App;
+
