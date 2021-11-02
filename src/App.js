61077dbb1dad962c78fe2ea617e7fb5c9216c1ec
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-01 21:01:37 -0700

    Connect The Reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useReducer }  from 'react';
+import reducer,  { initialState} from './reducers'
+import { addOne, applyNumber, changeOp } from './actions';
 
 import './App.css';
 
@@ -6,6 +8,24 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log('state', state);
+
+  const onClick = (e) => {
+    console.log('value XXX', e.target.innerText);
+    
+    switch(e.target.innerText) {
+      case("+"):
+          return dispatch(changeOp("+"));
+      case("-"):
+          return dispatch(changeOp("-"));
+      default:
+         return ;
+    }
+
+  }
 
   return (
     <div className="App">
@@ -24,37 +44,37 @@ function App() {
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick = {onClick} value={"M+"}/>
+              <CalcButton onClick = {onClick} value={"MR"}/>
+              <CalcButton onClick = {onClick} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick = {onClick} value={1}/>
+              <CalcButton onClick = {onClick} value={2}/>
+              <CalcButton onClick = {onClick} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick = {onClick} value={4}/>
+              <CalcButton onClick = {onClick} value={5}/>
+              <CalcButton onClick = {onClick} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick = {onClick} value={7}/>
+              <CalcButton onClick = {onClick} value={8}/>
+              <CalcButton onClick = {onClick} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick = {onClick} value={"+"}/>
+              <CalcButton onClick = {onClick} value={"*"}/>
+              <CalcButton onClick = {onClick} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick = {onClick} value={"CE"}/>
             </div>
 
           </form>
