0176836ea75f4b27d1fb7b12c8993c2428824cd7
RealSadLemon <esmodearburk@gmail.com>
2022-06-13 21:22:32 -0500

    First commit, answered Understanding-quesiton.md and imported required variables to create onClick function in App.js

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -2,6 +2,6 @@
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
 * 
-...
+...The onClick handler picks up the 'click' event and evokes the callback function I created, which executes the dispatch function. Within the dispatch function I passed through the corresponding key of the actions object. It finds the object from where I imported it from the actions index to decide which method to run for the given function. 
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, { initialState } from '../reducers';
 import './App.css';
-
+import * as actions from '../actions';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  const clickOnOne = () => {
+    dispatch(actions.addOne())
+  } 
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +22,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -29,7 +35,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={clickOnOne} />
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
