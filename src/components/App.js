274f205dee2be8899ee5d963e87bbfba9d8b7286
Aaron Belmore <belmoreaaron9@gmail.com>
2023-05-26 10:02:22 -0400

    Completed Clear operation

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import TotalDisplay from "./TotalDisplay";
 import CalcButton from "./CalcButton";
 import { initialState } from "../reducers";
-import { addOne, applyNumber, changeOperation } from "../actions";
+import { addOne, applyNumber, changeOperation, clearDisplay } from "../actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -71,7 +71,10 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton
+                onClick={() => dispatch(clearDisplay())}
+                value={"CE"}
+              />
             </div>
           </form>
         </div>
