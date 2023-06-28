df5c1e9e22987f18fdf9613621e7456bcce93dab
DJ1 <jimmynoble604@gmail.com>
2023-06-28 14:31:10 -0400

    Action index.js complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const MEMORY_ADD = "MEMORY_ADD";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_APPLY = "MEMORY_APPLY";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operation) => {
+    return({type: CHANGE_OPERATION, payload: operation})
+}
+
+export const clearDisplay = () => {
+    return({type: CLEAR_DISPLAY})
+}
+
+export const addMemory = () => {
+    return({type: MEMORY_ADD}) 
+}
+
+export const applyMemory = () => {
+    return({type: MEMORY_APPLY})
+}
+
+export const clearMemory = () => {
+    return({type: MEMORY_CLEAR})
 }
